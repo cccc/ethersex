17a8fe166cdb54dec64eb065f8c55f94250159ba
Philip Matura <ike@tura-home.de>
2014-10-01 00:13:34 +0200

    plenarsaal, wohnzimmer config

---
+++ services/autoc4/conf_plenarsaal.c
@@ -0,0 +1,47 @@
+#include "protocols/uip/uip.h"
+
+static autoc4_output_config outputs[] = {
+  { PORT_INDEX_A, 4, "licht/plenar/vornefenster" },
+  { PORT_INDEX_A, 5, "licht/plenar/vornewand" },
+  { PORT_INDEX_A, 6, "licht/plenar/hintenfenster" },
+  { PORT_INDEX_A, 7, "licht/plenar/hintenwand" },
+};
+static autoc4_input_config inputs[] = {
+  { PORT_INDEX_D, 2, "schalter/plenar/vorne", false, false },
+  { PORT_INDEX_D, 3, "fenster/plenar/vornerechts", true, false },
+  { PORT_INDEX_D, 4, "fenster/plenar/vornelinks", true, false },
+  { PORT_INDEX_D, 5, "fenster/plenar/hintenrechts", true, false },
+  { PORT_INDEX_D, 6, "fenster/plenar/hintenlinks", true, false },
+};
+static autoc4_dmx_config dmxs[] = {
+  {"dmx/plenar/vorne1",   1, 8},
+  {"dmx/plenar/vorne2",   9, 8},
+  {"dmx/plenar/vorne3",  17, 8},
+  {"dmx/plenar/hinten1", 25, 8},
+  {"dmx/plenar/hinten2", 33, 8},
+  {"dmx/plenar/hinten3", 41, 8},
+  {"dmx/plenar/hinten4", 49, 8},
+};
+static char const* const auto_subscribe_topics[] = { "licht/plenar/+","dmx/plenar/+","dmx/plenar", NULL };
+static mqtt_connection_config_t mqtt_config = {
+  .client_id = "wohnzimmer",
+  .user = NULL,
+  .pass = NULL,
+  .will_topic = NULL,
+  .will_qos = 0,
+  .will_retain = 0,
+  .will_message = NULL,
+  .target_ip = { HTONS(((172) << 8) | (23)), HTONS(((23) << 8) | (110)) },
+
+  .auto_subscribe_topics = auto_subscribe_topics,
+};
+static autoc4_config_t config = {
+  .output_count = 4,
+  .input_count = 5,
+  .dmx_count = 7,
+  .output_configs = outputs,
+  .input_configs = inputs,
+  .dmx_configs = dmxs,
+  .dmx_topic = "dmx/plenar",
+  .mqtt_con_config = &mqtt_config,
+};
---
+++ services/autoc4/conf_wohnzimmer.c
@@ -0,0 +1,39 @@
+#include "protocols/uip/uip.h"
+
+static autoc4_output_config outputs[] = {
+  { PORT_INDEX_C, 3, "licht/wohnzimmer/kueche" },
+  { PORT_INDEX_C, 4, "licht/wohnzimmer/mitte" },
+  { PORT_INDEX_C, 5, "licht/wohnzimmer/tuer" },
+};
+static autoc4_input_config inputs[] = {
+  { PORT_INDEX_C, 6, "schalter/wohnzimmer/links", false, false },
+  { PORT_INDEX_C, 7, "schalter/wohnzimmer/rechts", false, false },
+};
+static autoc4_dmx_config dmxs[] = {
+  {"dmx/wohnzimmer/mitte1",   1, 8},
+  {"dmx/wohnzimmer/mitte2",   9, 8},
+  {"dmx/wohnzimmer/mitte3",  17, 8},
+};
+static char const* const auto_subscribe_topics[] = { "licht/wohnzimmer/+","dmx/wohnzimmer/+","dmx/wohnzimmer", NULL };
+static mqtt_connection_config_t mqtt_config = {
+  .client_id = "wohnzimmer",
+  .user = NULL,
+  .pass = NULL,
+  .will_topic = NULL,
+  .will_qos = 0,
+  .will_retain = 0,
+  .will_message = NULL,
+  .target_ip = { HTONS(((172) << 8) | (23)), HTONS(((23) << 8) | (110)) },
+
+  .auto_subscribe_topics = auto_subscribe_topics,
+};
+static autoc4_config_t config = {
+  .output_count = 3,
+  .input_count = 2,
+  .dmx_count = 3,
+  .output_configs = outputs,
+  .input_configs = inputs,
+  .dmx_configs = dmxs,
+  .dmx_topic = "dmx/wohnzimmer",
+  .mqtt_con_config = &mqtt_config,
+};
